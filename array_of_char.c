1e9a46d21897e6f41280195104c8fa0901414154
bsnu002 <bishnunath200@gmail.com>
2023-04-23 20:32:42 +0600

    what is string

---
+++ array_of_char.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main() {
+    // char a[5];
+    // for (int i=0; i<5; i++){
+    //     scanf("%c", &a[i]);
+    // }
+
+    // for (int i=0; i < 5; i++){
+    //     printf("%c\n", a[i]);
+    // }
+
+    int a[5];
+
+    int sz = sizeof(a) / sizeof(int);
+
+    // printf("%d", sizeof(int));
+
+    printf("%d", sz);
+
+    return 0;
+}
